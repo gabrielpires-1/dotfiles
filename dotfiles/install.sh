b30764dbca8fbe2795fcb69caf74cc50841d378b
Gabriel Pires <gabrielpires2991@gmail.com>
2026-03-12 19:03:38 -0300

    feat: add first alias

---
+++ dotfiles/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Get the absolute path of the directory where THIS script is located
+DOTFILES_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+BASHRC="$HOME/.bashrc"
+ALIASES_PATH="$DOTFILES_DIR/shell/aliases.sh"
+
+echo "Installing Bash Powerup from: $DOTFILES_DIR"
+
+if ! grep -q "$ALIASES_PATH" "$BASHRC"; then
+    echo -e "\n# Load custom aliases\n. $ALIASES_PATH" >> "$BASHRC"
+    echo "Installation complete."
+else
+    echo "Aliases already linked in .bashrc."
+fi
---
+++ dotfiles/shell/aliases.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Aliases for commonly used commands
+alias c='clear'
+alias ll='ls -alF'
+alias la='ls -A'
+
+# Custom alias to navigate to specific directories
+alias cdworkspace='cd "$HOME/Desktop/workspace"'
