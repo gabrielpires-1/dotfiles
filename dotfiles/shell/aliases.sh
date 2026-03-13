cc9cbc5fcf5c714c413529c425255a885afbe5bb
Gabriel Pires <gabrielpires2991@gmail.com>
2026-03-13 12:41:23 -0300

    feat: add git aliases

--- dotfiles/shell/aliases.sh
+++ dotfiles/shell/aliases.sh
@@ -5,8 +5,35 @@
 
 # Aliases for commonly used commands
 alias c='clear'
-alias ll='ls -alF'
+alias ll='ls -lF'
 alias la='ls -A'
 
 # Custom alias to navigate to specific directories
 alias cdworkspace="cd \"$WORKSPACE_PATH\""
+
+# Git aliases
+alias g='git'
+alias gs='git status -s -b'
+alias ga='git add'
+alias gc='git commit -m'
+alias gsw='git switch'
+alias gswc='git switch -c'
+alias gd='git diff'
+alias gl='git log --oneline --graph --decorate'
+alias gu='git pull'
+
+# Custom function to prevent accidental pushes to main/master
+gp() {
+  local branch=$(git branch --show-current)
+  
+  if [[ "$branch" == "main" || "$branch" == "master" ]]; then
+    echo "❌ Erro: Push direto na '$branch' bloqueado por segurança!"
+    echo "Use 'git push' manualmente se realmente precisar disso."
+    return 1
+  fi
+
+  git push -u origin "$branch"
+}
+
+
+
