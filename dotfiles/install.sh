a440162aa85e38bfd0e831dc77c1ad6e805036a1
Gabriel Pires <gabrielpires2991@gmail.com>
2026-03-13 11:02:25 -0300

    fix: quote aliases path to support special characters

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -8,7 +8,7 @@ ALIASES_PATH="$DOTFILES_DIR/shell/aliases.sh"
 echo "Installing Bash Powerup from: $DOTFILES_DIR"
 
 if ! grep -q "$ALIASES_PATH" "$BASHRC"; then
-    echo -e "\n# Load custom aliases\n. $ALIASES_PATH" >> "$BASHRC"
+    echo -e "\n# Load custom aliases\n. \"$ALIASES_PATH\"" >> "$BASHRC"
     echo "Installation complete."
 else
     echo "Aliases already linked in .bashrc."
