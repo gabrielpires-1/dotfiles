9800416c4e097c9201266dfd2994632f18baebed
Gabriel Pires <gabrielpires2991@gmail.com>
2026-03-13 11:06:12 -0300

    feat: constants file to match user's organization

--- dotfiles/shell/aliases.sh
+++ dotfiles/shell/aliases.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# Source constants
+. "$(dirname "${BASH_SOURCE[0]}")/constants.sh"
+
 # Aliases for commonly used commands
 alias c='clear'
 alias ll='ls -alF'
 alias la='ls -A'
 
 # Custom alias to navigate to specific directories
-alias cdworkspace='cd "$HOME/Desktop/workspace"'
+alias cdworkspace="cd \"$WORKSPACE_PATH\""
---
+++ dotfiles/shell/constants.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Constants file for dotfiles configuration
+# Update these paths according to your project structure and folder organization
+
+# Main workspace directory
+WORKSPACE_PATH="$HOME/Desktop/workspace"
